6a0b2e69a74f7c47fa7e567beaf9947ad2af5d28
Rob Fraley <robertdfraley@hotmail.com>
2023-10-14 18:48:15 -0400

    build 1 of assignment -- saving progress

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,13 @@
--- Add your sql here
+-- SELECT * from subscriptions;
+-- SELECT * from customers;
+-- SELECT * from orders;
+
+
+WITH subs AS (
+    select subscription_id from subscriptions WHERE description = 'Fashion Magazine'
+)
+SELECT DISTINCT c.customer_name
+FROM customers c, orders o
+WHERE c.customer_id = o.customer_id
+AND o.order_status = 'unpaid'
+AND o.subscription_id in subs;
